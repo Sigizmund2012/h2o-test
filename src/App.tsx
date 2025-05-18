bfcd503cd0079c1a65732dd3dbf7b2e8f2453875
Sergey Segeda <segeda823@mail.ru>
2025-05-18 16:05:34 +0300

    Initial boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,30 @@
+import "./Sidebar.scss";
+export default function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar__logo"></div>
+      <nav className="sidebar__navigation">
+        <ul>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+}
