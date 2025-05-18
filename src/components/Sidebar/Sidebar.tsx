8b48ceb9a4bd48252b38072a579f4caded3a5515
Sergey Segeda <segeda823@mail.ru>
2025-05-18 21:10:02 +0300

    Create Sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,26 +2,29 @@ import "./Sidebar.scss";
 export default function Sidebar() {
   return (
     <aside className="sidebar">
-      <div className="sidebar__logo"></div>
-      <nav className="sidebar__navigation">
-        <ul>
-          <li>
-            <a href=""></a>
+      <div className="logo"></div>
+      <nav className="navigation">
+        <ul className="navigation__list">
+          <li className="navigation__list-item">
+            <a className="navigation_calendar" href="#"></a>
           </li>
-          <li>
-            <a href=""></a>
+          <li className="navigation__list-item">
+            <a className="navigation_list" href="#"></a>
           </li>
-          <li>
-            <a href=""></a>
+          <li className="navigation__list-item">
+            <a className="navigation_mailbox" href="#"></a>
           </li>
-          <li>
-            <a href=""></a>
+          <li className="navigation__list-item">
+            <a className="navigation_team" href="#"></a>
           </li>
-          <li>
-            <a href=""></a>
+          <li className="navigation__list-item">
+            <a className="navigation_database" href="#"></a>
           </li>
-          <li>
-            <a href=""></a>
+          <li className="navigation__list-item navigation_active">
+            <a className="navigation_graph" href="#"></a>
+          </li>
+          <li className="navigation__list-item">
+            <a className="navigation_settings" href="#"></a>
           </li>
         </ul>
       </nav>
