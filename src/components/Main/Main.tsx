f04b85d1ba3fcdf5a987f92f25b3e1086e6c2f00
Sergey Segeda <segeda823@mail.ru>
2025-05-19 13:45:02 +0300

    Implement css grid template

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,30 @@
 import "./Main.scss";
 export default function Main() {
-  return <main>Main</main>;
+  return (
+    <>
+      <div className="header-navigation">
+        <div className="arrows">
+          <div className="arrow-left">Левая стрелка</div>
+          <div className="arrow-right">Правая стрелка</div>
+        </div>
+      </div>
+      <div className="header-tabs">
+        <div className="header-tabs__tab">
+          <a href="">Сводный отчёт внутри компании</a>
+        </div>
+        <div className="header-tabs__tab">
+          <a href="">Сводный отчёт внутри компании</a>
+        </div>
+        <div className="header-tabs__tab">
+          <a href="">Сводный отчёт внутри компании</a>
+        </div>
+      </div>
+      <div className="header-user">
+        <div className="header-user__photo"></div>
+        <div className="header-user__name">Имя пользователя</div>
+        <div className="header-user__position">менеджер продаж</div>
+      </div>
+      <div className="main-content">Блоки</div>
+    </>
+  );
 }
