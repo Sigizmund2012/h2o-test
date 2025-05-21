d277437045c2599197b5acd1192b23f51290191e
Sergey Segeda <segeda823@mail.ru>
2025-05-21 17:03:37 +0300

    Create summary reports blocks boilerplate

--- src/components/SummaryReport/SummaryReport.tsx
+++ src/components/SummaryReport/SummaryReport.tsx
@@ -1,4 +1,13 @@
 import "./SummaryReport.scss";
 export default function SummaryReport() {
-  return <div className="summary-report">SummaryReport</div>;
+  return (
+    <div className="summary-report">
+      <h1 className="heading">Сводный отчет</h1>
+      <div className="total">Сумма</div>
+      <div className="b2b">b2b</div>
+      <div className="b2c">b2c</div>
+      <div className="problems">Проблемы</div>
+      <div className="graph">График</div>
+    </div>
+  );
 }
