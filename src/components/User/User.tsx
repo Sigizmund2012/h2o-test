0d156b4135202ec012391f10bd60fd24bceb4750
Sergey Segeda <segeda823@mail.ru>
2025-05-21 12:10:05 +0300

    Create user component

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -2,9 +2,19 @@ import "./User.scss";
 export default function User() {
   return (
     <div className="user">
-      <div className="user__photo"></div>
-      <div className="user__name">Имя пользователя</div>
-      <div className="user__position">менеджер продаж</div>
+      <div className="user__photo">
+        <img src="/user-photo.png" alt="User" />
+      </div>
+      <div className="user__badge">
+        <div className="user__name">Kristina 🐰</div>
+        <div className="user__position">менеджер продаж</div>
+      </div>
+      <a className="user__dropdown" href="#"></a>
+      <div className="user__profile">
+        <a href="#">Профиль</a>
+        <a href="#">Настройки</a>
+        <a href="#">Выйти</a>
+      </div>
     </div>
   );
 }
